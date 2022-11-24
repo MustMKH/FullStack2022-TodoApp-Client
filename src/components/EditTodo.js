6c368008338505b6ba5bf5a52dfdf3fda467f772
MustMKH <mustonen.katri@yahoo.com>
2022-11-24 08:41:25 +0200

    Todo app client

---
+++ src/components/EditTodo.js
@@ -0,0 +1,86 @@
+import { Fragment, useState } from 'react'
+
+// TODO: The Bootstrap modal close button does not work
+
+const EditTodo = ({ todo }) => {
+    const [description, setDescription] = useState(todo.description)
+
+    // - - - Edit description - - -
+    const updateDescription = async (event) => {
+        event.preventDefault()
+        try {
+            const body = { description }
+            const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            })
+            window.location = "/" // Updates the data (page refreshes and the new data is displayed instead of the old data)
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+  return (
+    <Fragment>
+        {/* Modal copied from bootstrap: */}
+        <button
+            type="button"
+            class="btn btn-warning"
+            data-bs-toggle="modal"
+            data-bs-target={`#id${todo.todo_id}`}
+        >
+            Edit
+        </button>
+        <div
+            class="modal"
+            id={`id${todo.todo_id}`}
+            onClick={() => setDescription(todo.description)}
+        >
+            <div class = "modal-dialog">
+                <div class = "modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Edit Todo</h4>
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            onClick={() => setDescription(todo.description)}
+                        >
+                            &times
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={description}
+                            onChange={event => setDescription(event.target.value)}
+                        />
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-warning"
+                            data-dismiss="modal"
+                            onClick={event => updateDescription(event)}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            type="button"
+                            class="btn btn-danger"
+                            data-dismiss="modal"
+                            onClick={() => setDescription(todo.description)}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Fragment>
+  )
+}
+
+export default EditTodo
